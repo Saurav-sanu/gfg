3ca6a205b334083d5121ed2258896551891f1e1f
Saurav-sanu <142467513+Saurav-sanu@users.noreply.github.com>
2025-02-26 11:46:32 +0530

    Update topological-sort.cpp

--- Difficulty: Medium/Topological sort/topological-sort.cpp
+++ Difficulty: Medium/Topological sort/topological-sort.cpp
@@ -98,3 +98,49 @@ int main() {
     return 0;
 }
 // } Driver Code Ends
+
+
+
+//using bfs -- and it is alos called kahn's algorithm
+
+
+vector<int> topologicalSort(vector<vector<int>>& adj) {
+        // using BFS
+        int n=adj.size();
+        vector<int>result;
+        vector<bool>visited(n,false);
+        vector<int>indegree(n,0);
+        
+        queue<int>q;
+        
+        //step 1
+        
+        for(int u=0;u<n;u++){
+            for(int &v:adj[u]){
+                indegree[v]++;
+            }
+        }
+        
+        // step2
+        
+        for(int i=0;i<n;i++){
+            if(indegree[i]==0){
+                q.push(i);
+            }
+        }
+        
+        // step3
+        while(!q.empty()){
+            int element=q.front();
+            q.pop();
+            result.push_back(element);
+            
+            for(int &v:adj[element]){
+                indegree[v]--;
+                if(indegree[v]==0){
+                    q.push(v);
+                }
+            }
+        }
+        return result;
+    }
