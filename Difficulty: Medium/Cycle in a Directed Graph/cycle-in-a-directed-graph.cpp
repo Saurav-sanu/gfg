96cc70f5cd189744f4b66230f4685db382dab60e
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-05 23:37:15 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
+++ Difficulty: Medium/Cycle in a Directed Graph/cycle-in-a-directed-graph.cpp
@@ -7,49 +7,36 @@ using namespace std;
 
 class Solution {
   public:
-    // Function to detect cycle in a directed graph.
+    bool isCycleDfs(vector<bool>&visited,vector<bool>&inRecursion,int u,vector<vector<int>>&adj){
+        visited[u]=true;
+        inRecursion[u]=true;
+        
+        
+        for(int &v:adj[u]){
+            if(visited[v]==false && isCycleDfs(visited,inRecursion,v,adj)){
+                return true;
+            }
+            
+            else if(inRecursion[v]==true && visited[v]==true){
+                return true;
+            }
+        }
+        inRecursion[u]=false;
+        return false;
+        
+        
+    }
     bool isCyclic(vector<vector<int>> &adj) {
-        
-        int count=0;
         int n=adj.size();
-        queue<int>q;
-        vector<int>indegree(n,0);
+        vector<bool>visited(n,false);
+        vector<bool>inRecursion(n,false);
         
-        //step1
-        for(int u=0;u<n;u++){
-            for(int &v:adj[u]){
-                indegree[v]++;
-            }
-        }
-        
-        //step2
         for(int i=0;i<n;i++){
-            if(indegree[i]==0){
-                q.push(i);
-                count++;
+            if(!visited[i] && isCycleDfs(visited,inRecursion,i,adj)){
+                return true;
             }
         }
-        
-        //step 3
-        while(!q.empty()){
-            int u=q.front();
-            q.pop();
-            for(int &v:adj[u]){
-                indegree[v]--;
-                if(indegree[v]==0){
-                    q.push(v);
-                    count++;
-                }
-            }
-        }
-        
-        if(count==n){
-            return false;
-        }
-        else {
-            return true;
-        }
-        
+        return false;
     }
 };
 
