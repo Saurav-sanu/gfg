8ba18bdeec9bb1bf1439423d42a6865775c78559
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-03-22 23:26:55 +0530

    Update nth-fibonacci-number.cpp

--- Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
+++ Difficulty: Easy/Nth Fibonacci Number/nth-fibonacci-number.cpp
@@ -23,20 +23,35 @@ class Solution {
 };
 
 
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        Solution ob;
-        cout << ob.nthFibonacci(n) << endl;
+//usingn dp
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
 
-        cout << "~"
-             << "\n";
-    }
-    return 0;
+
+// User function Template for C++
+class Solution {
+  public:
+  int solve(int n,vector<int>&dp){
+        if(n==0){
+            return 0;
+        }
+        if(n==1){
+            return 1;
+        }
+
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+
+        return dp[n]=solve(n-1,dp)+solve(n-2,dp);
+
 }
+    int nthFibonacci(int n) {
+       
+        vector<int>dp(n+1,-1);
+        return solve(n,dp);
+    }
+};
 
-// } Driver Code Ends
