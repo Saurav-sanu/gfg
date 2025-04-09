c9605d72c1150735eb983d154ce5c2403b37d4cc
Saurav <142467513+Saurav-sanu@users.noreply.github.com>
2025-04-09 14:09:23 +0530

    Update shortest-common-supersequence.cpp

--- Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
+++ Difficulty: Medium/Shortest Common Supersequence/shortest-common-supersequence.cpp
@@ -101,6 +101,39 @@ class Solution {
         int n=s2.length();
         vector<vector<int>>t(m+1,vector<int>(n+1,-1));
         return solve(m,n,s1,s2,t);
+
+
+
+
+
+    //bottum up appraoach
+      class Solution {
+  public:
+  
+    //bottum up
+    int shortestCommonSupersequence(string &s1, string &s2) {
+        int m=s1.length();
+        int n=s2.length();
+        vector<vector<int>>t(m+1,vector<int>(n+1));
+        for(int i=0;i<=m;i++){
+            for(int j=0;j<=n;j++){
+                if(i==0||j==0){
+                    t[i][j]=i+j;
+                }
+                else if(s1[i-1]==s2[j-1]){
+                    t[i][j]=1+t[i-1][j-1];
+                }
+                else{
+                    t[i][j]=1+min(t[i-1][j],t[i][j-1]);
+                }
+            }
+        }
+        
+        return t[m][n];
+        
+    }
+};
+
     }
 };
 // } Driver Code Ends
