d98911c689d5b9592fc1da3b2e357a20217a061f
Saurav <sauravsanu100@gmail.com>
2025-08-09 16:53:15 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Add 1 to a Linked List Number/add-1-to-a-linked-list-number.cpp
@@ -0,0 +1,42 @@
+/*
+
+struct Node
+{
+    int data;
+    struct Node* next;
+
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+*/
+
+class Solution {
+  public:
+  int helper(Node* temp){
+      //base case
+      if(temp==NULL){
+          return 1;
+      }
+      int carry=helper(temp->next);
+      temp->data=temp->data+carry;
+      if(temp->data<10){
+          return 0;
+      }
+      temp->data=0;
+      return 1;
+  }
+    Node* addOne(Node* head) {
+        // Your Code here
+        int  carry=helper(head);
+        if(carry==1){
+            Node* newNode=new Node(1);
+            newNode->next=head;
+            return newNode;
+        }
+        return head;
+        
+    }
+};
