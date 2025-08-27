e997882172e905472ef2f4d3c376c482ad2909a7
Saurav <sauravsanu100@gmail.com>
2025-08-27 06:04:48 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
+++ Difficulty: Easy/Disjoint set (Union-Find)/disjoint-set-union-find.cpp
@@ -1,58 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-int find(int a[], int x);
-void unionSet(int a[], int x, int z);
-
-int main() {
-    int t;
-    cin >> t;
-    while (t--) {
-        int n;
-        cin >> n;
-        int a[n + 1];
-        for (int i = 1; i <= n; i++)
-            a[i] = i;
-        int k;
-        cin >> k;
-        for (int i = 0; i < k; i++) {
-            string s;
-            cin >> s;
-            if (s == "UNION") {
-                int z, x;
-                cin >> x >> z;
-                unionSet(a, x, z);
-            } else {
-                int x;
-                cin >> x;
-                int parent = find(a, x);
-                cout << parent << " ";
-            }
-        }
-        cout << endl;
-
-        cout << "~"
-             << "\n";
-    }
-}
-// } Driver Code Ends
-
-
 /*Complete the functions below*/
+
 int find(int par[], int x) {
-    
     if(x==par[x]){
         return x;
     }
-    
-    return find(par,par[x]);
+    return par[x]=find(par,par[x]);
 }
 
 void unionSet(int par[], int x, int z) {
+    ///by ra
+    
     int x_parent=find(par,par[x]);
     int y_parent=find(par,par[z]);
     
     if(x_parent!=y_parent){
         par[x_parent]=y_parent;
+        
     }
+   
 }
+
+
