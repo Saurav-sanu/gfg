6195681587ddc92d237a839346a070814780395f
Saurav <sauravsanu100@gmail.com>
2025-11-07 04:02:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
+++ Difficulty: Medium/Flattening a Linked List/flattening-a-linked-list.cpp
@@ -1,53 +1,57 @@
-/* Node structure  used in the program
-
-struct Node{
+/*
+class Node {
+public:
     int data;
-    struct Node * next;
-    struct Node * bottom;
+    Node* next;
+    Node* bottom;
 
-    Node(int x){
+    Node(int x) {
         data = x;
         next = NULL;
         bottom = NULL;
     }
-
 };
 */
 
 class Solution {
   public:
-  
   Node* merge(Node* head1,Node* head2){
       //base case
       if(head1==NULL){
           return head2;
+          
       }
       if(head2==NULL){
           return head1;
       }
+      
       Node* result;
+      
       if(head1->data<=head2->data){
           result=head1;
-          result->bottom=merge(result->bottom,head2);
+          
+          result->bottom=merge(head1->bottom,head2);
       }
       else{
           result=head2;
-          result->bottom=merge(head1,result->bottom);
+          
+          result->bottom=merge(head1,head2->bottom);
       }
+      
       return result;
   }
-  
-    // Function which returns the  root of the flattened linked list.
     Node *flatten(Node *root) {
-        // Your code here
+        // code here
+        
         if(root==NULL){
-            return NULL;
-            
+            return root;
         }
         
-        Node* head2=flatten(root->next);
+        Node* nextkahead=flatten(root->next);
         
-       return  merge(root,head2);
+        
+        
+        return merge(root,nextkahead);
         
     }
 };
